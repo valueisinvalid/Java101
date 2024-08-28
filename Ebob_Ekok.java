2639528dac3ef930b28e8cb32a826fc0e7d6cd10
valueisinvalid <renoeportn86@gmail.com>
2024-08-28 19:36:00 +0300

    Ebob Ekok

---
+++ Ebob_Ekok.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Ebob_Ekok {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n1 = input.nextInt();
+        int n2 = input.nextInt();
+
+        int ebob=1;
+        int ekok=1;
+
+        for(int i =1; i<= n1; i++){
+            if(n1%i==0 && n2%i==0){
+                ebob = i;
+            }
+        }
+
+        System.out.println(ebob);
+
+        //ekok
+        for(int i =1; i<= (n1*n2); i++){
+            if(i%n1==0 && i%n2==0){
+                ekok = i;
+                break;
+            }
+
+        }
+
+        System.out.println(ekok);
+    }
+}
