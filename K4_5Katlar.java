05aacb17d3fd766dd0a8d5fdba502e1800b22a19
valueisinvalid <renoeportn86@gmail.com>
2024-08-28 16:11:33 +0300

    4 ve 5in katlarını yazdıran program

---
+++ K4_5Katlar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class K4_5Katlar {
+    public static void main(String[] args) {
+
+        int n =1;
+                for(int i=1; n<=i; i*=4){
+
+                    System.out.println(i);
+                }
+
+                for(int i=1; n<=i; i*=5){
+
+                    System.out.println(i);
+                 }
+    }
+}
