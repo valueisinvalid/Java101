ac7d4366d24d560d26386bc6531e54e2afae5632
valueisinvalid <renoeportn86@gmail.com>
2024-09-20 15:18:31 +0300

    ters üçgen

---
+++ TersUcgen.java
@@ -0,0 +1,13 @@
+public class TersUcgen {
+    public static void main(String[] args) {
+
+        for(int c =10; c>=1 ; c--){
+            for(int r= c; r>=1; r--){
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
+
+    }
+}
